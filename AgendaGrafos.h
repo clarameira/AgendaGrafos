8020a554c1c571a378b55a6669d6023476710fe6
Maria Clara Meira <138517618+clarameira@users.noreply.github.com>
2023-10-06 19:05:17 -0300

    Delete AgendaGrafos.h

--- AgendaGrafos.h
+++
@@ -1,7 +0,0 @@
-#include "AgendaGrafos.c"
-
-struct Vertice* grafo[CONTATOS];
-
-void adicionarVertice(char *nome, char *celular, char *email);
-
-int verificarRelacao(char *nomeContato1, char *nomeContato2);
